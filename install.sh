b01dd22e31fc701d64bfa764f8dc0d6ce30ae931
d.kanehira <disc99.o@gmail.com>
2016-04-26 00:01:17 +0900

    Change installer

--- install.sh
+++ install.sh
@@ -1,5 +1,12 @@
 #! /bin/bash
-ln -s ~/dotfiles/.tmux.conf ~/.tmux.conf
-ln -s ~/dotfiles/.vimrc ~/.vimrc
-ln -s ~/dotfiles/.tigrc ~/.tigrc
+#ln -s ~/dotfiles/.tmux.conf ~/.tmux.conf
+#ln -s ~/dotfiles/.vimrc ~/.vimrc
+#ln -s ~/dotfiles/.tigrc ~/.tigrc
 
+
+for f in .??*
+do
+    [[ "$f" == ".git" ]] && continue
+    [[ "$f" == ".DS_Store" ]] && continue
+    echo "$f"
+done
